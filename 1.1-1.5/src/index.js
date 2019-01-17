b19654ee89c42245b047a2ad365c9b30cfbca28c
Gert Adamson <adamson.gert@hotmail.com>
2019-01-17 20:02:25 +0200

    Tehtävät 1.1 - 1.5

---
+++ 1.1-1.5/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Part = (props) => (
+  <>
+    <p>{props.part.name} {props.part.exercises}</p>
+  </>
+)
+
+const Header = (props) => (
+  <>
+    <h1>{props.course}</h1>
+  </>
+)
+
+const Content = (props) => (
+  <>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </>
+)
+
+const Total = (props) => (
+  <>
+    <p>
+      yhteensä {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} tehtävää
+    </p>
+  </>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
