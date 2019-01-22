c788014f9e659af66f56dadf25d7aec6aced3500
Gert Adamson <adamson.gert@hotmail.com>
2019-01-22 22:35:46 +0200

    Tehtävä 1.14

--- anekdootit/src/index.js
+++ anekdootit/src/index.js
@@ -3,27 +3,39 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-  const [vote, setVote] = useState(selected)
+  const [mostVotes, setMostVotes] = useState(0)
 
-  let min = Math.ceil(0)
-  let max = Math.floor(props.anecdotes.length - 1)
-  let random = Math.floor(Math.random() * (max - min + 1)) + min
+  if (props.anecdotes[selected].votes > props.anecdotes[mostVotes].votes) {
+    setMostVotes(selected)
+  }
   
-  const handleSelectedClick = () => setSelected(random)
+  const handleSelectedClick = () => setSelected(randomNumber(props.anecdotes.length, selected))
   const handleVoteClick = () => {
     props.anecdotes[selected].votes += 1
-    setVote(props.anecdotes[selected])
+    setSelected(selected)
   }
 
   return (
     <div>
-      <p>{props.anecdotes[selected].anecdote}</p>
-      <p>has {props.anecdotes[selected].votes} votes</p>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={props.anecdotes[selected].text} votes={props.anecdotes[selected].votes} />
       <Button text='vote' handleClick={handleVoteClick} />
       <Button text='next anectode' handleClick={handleSelectedClick} />
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdote={props.anecdotes[mostVotes].text} votes={props.anecdotes[mostVotes].votes} />
     </div>
   )
 }
+
+const Anecdote = ({anecdote, votes}) => (
+  <div>
+    <p>
+      {anecdote}
+      <br></br>
+      has {votes} votes
+    </p>
+  </div>
+)
 
 const Button = ({text, handleClick}) => (
   <button onClick={handleClick}>
@@ -31,29 +43,38 @@ const Button = ({text, handleClick}) => (
   </button>
 )
 
+const randomNumber = (length, selected) => {
+  let min = Math.ceil(0)
+  let max = Math.floor(length - 1)
+  let random = Math.floor(Math.random() * (max - min + 1)) + min
+
+  if (random === selected) return randomNumber(length, selected)
+  else return random
+}
+
 const anecdotes = [
   {
-    anecdote: 'If it hurts, do it more often',
+    text: 'If it hurts, do it more often',
     votes: 0
   },
   {
-    anecdote: 'Adding manpower to a late software project makes it later!',
+    text: 'Adding manpower to a late software project makes it later!',
     votes: 0
   },
   {
-    anecdote: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    text: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     votes: 0
   },
   {
-    anecdote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     votes: 0
   },
   {
-    anecdote: 'Premature optimization is the root of all evil.',
+    text: 'Premature optimization is the root of all evil.',
     votes: 0
   },
   {
-    anecdote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    text: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     votes: 0
   }
 ]
